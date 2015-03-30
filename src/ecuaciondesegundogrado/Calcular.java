8a99e8e7f248ed18bea21e0d2622cc05fa5dd630
wilfri95 <wilfri_95@hotmail.com>
2015-03-29 21:21:40 -0300

    refinado control de flujo

--- src/ecuaciondesegundogrado/Calcular.java
+++ src/ecuaciondesegundogrado/Calcular.java
@@ -10,8 +10,9 @@ public class Calcular {
     private double c;
     private double resultado1;
     private double resultado2;
-    private double raizCuadrada;
+    private double imaginario;
     double dentroRaiz;
+    private boolean seGrafica;
 
     public Calcular() {
     }
@@ -25,44 +26,40 @@ public class Calcular {
 
     public void calcular() {
 
-        if (a != 0) { //'a' debe ser distinto de cero para que la ecuación no sea una constante 
-            double bCuadrado = Math.pow(b, 2);//elevar b al cuadrado
-            double raiz = Math.sqrt(bCuadrado - 4 * a * c); //raiz cuadrada
-             dentroRaiz= bCuadrado - (4 * a * c);
-
-//             se calculan los valores de x (x1 y x2) solo si la raiz es positiva
-//             de lo contrario cambia el valor de un Label advirtiendo que la solución
-//             es imaginaria
-            if (dentroRaiz >= 0) {
-
-                //resolver ecuación de segundo grado
-                resultado1 = ((-(b) + (raiz)) / (2 * a));//primera solución (x1)
-                resultado1 = ((int) (resultado1 * 10) / 10.0);//reducción de decimales
-                resultado2 = ((-(b) - (raiz)) / (2 * a));//segunda solución(x2)
-                resultado2 = ((int) (resultado2 * 10) / 10.0);//reducción de decimales
-
-                editarLabels("" + resultado1, "" + resultado2, "");
-
-            } else {
-//              Raiz negativa solución pertenece a los imaginarios
-                editarLabels("", "", "Imaginario");
-            }
-        } else {
-            editarLabels("", "", "'A' no puede ser cero.");
+        seGrafica = false;
+        double bCuadrado = Math.pow(b, 2);//elevar b al cuadrado
+        double raiz = Math.sqrt(bCuadrado - 4 * a * c); //calcular raiz cuadrada
+        dentroRaiz = bCuadrado - (4 * a * c);
+        
+        if ((a == 0) && (b == 0)) {
+            editarLabels("", "", "No tiene solución Real");
+        } else if ((a == 0) && (b != 0)) {//'a' debe ser distinto de cero para que la ecuación no sea una constante
+            editarLabels("" + (-(c / b)), "", "La ecuación no es cuadrática");
+        } else if (dentroRaiz < 0) {//discriminante negativo= 2 soluciiones complejas
+            imaginario= Math.sqrt((-dentroRaiz))/(2*a);
+            editarLabels("" + (-(b / (2 * a)))+" + i"+imaginario, "" + (-(b / (2 * a)))+" - i"+imaginario, " 2 soluciones complejas");
+        } else if (dentroRaiz > 0) {//discriminante positivo
+            //resolver ecuación de segundo grado
+            resultado1 = ((-(b) + (raiz)) / (2 * a));//primera solución (x1)
+            resultado2 = ((-(b) - (raiz)) / (2 * a));//segunda solución(x2)
+            resultado1 = ((int) (resultado1 * 10) / 10.0);//reducción de decimales
+            resultado2 = ((int) (resultado2 * 10) / 10.0);//reducción de decimales
+            editarLabels("" + resultado1, "" + resultado2, "2 soluciones reales");
+            seGrafica = true;
+        } else if (dentroRaiz == 0) {//discriminante igual a cero
+            editarLabels("" + (-(b / 2 * a)), "", "1 solución real");
         }
     }
 
     public void graficar() {
-
-        if (dentroRaiz<0) {
-
-            JOptionPane.showMessageDialog(null, "La solución no pertenece a los números REALES");
-        } else {
+        if (seGrafica == true) {
 //          se crea una instancia de la clase Grafico
             final Grafico demo = new Grafico("Ecuacion Cuadratica", this.a, this.b, this.c);
             demo.pack();
             RefineryUtilities.centerFrameOnScreen(demo);
             demo.setVisible(true);
+        } else {
+            editarLabels("", "", "La solución no se puede graficar");
         }
 
     }
--- src/ecuaciondesegundogrado/Test_Ecuacion.java
+++ src/ecuaciondesegundogrado/Test_Ecuacion.java
@@ -3,9 +3,11 @@ package ecuaciondesegundogrado;
 public class Test_Ecuacion {
 
     public static void main(String[] args) {
-       
-        Ecuacion a = new Ecuacion ();
+
+        Ecuacion a = new Ecuacion();
+        a.setLocationRelativeTo(null);//Centrar ventana
+        a.setResizable(false);//inhabilitar maximizar ventana
         a.setVisible(true);
     }
-    
+
 }
